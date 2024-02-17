26bf3e440d0bed7873dfab8622b0ab0163b2192d
Kavya-gopi <kavijanu1121@gmail.com>
2024-02-17 18:59:48 +0530

    ifElse

--- if-else/ifelse.js
+++ if-else/ifelse.js
@@ -1,20 +1,15 @@
-let password_correct=true;
-if(password_correct)
-   console.log("Your are logged In");
-else
-   console.log("Incorrect Password");
+let password_correct = true;
+if (password_correct) console.log("Your are logged In");
+else console.log("Incorrect Password");
 console.log("Bye");
 
-let a=30;
-let b=35;
+let a = 30;
+let b = 35;
 let max;
 
-if(a>b)
-   max=a;
-else
-   max=b;
+if (a > b) max = a;
+else max = b;
 console.log(max);
 //ternary operator
-max=a>b?a:b;
+max = a > b ? a : b;
 console.log(max);
-
