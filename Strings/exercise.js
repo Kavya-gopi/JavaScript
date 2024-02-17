55fdac6970b6e985828c5b441d4b4564d127bb45
Kavya-gopi <kavijanu1121@gmail.com>
2024-02-17 19:02:46 +0530

    StringsDemo

--- Strings/exercise.js
+++ Strings/exercise.js
@@ -1,4 +1,4 @@
 /*Print a random letter from your name */
-let name='Kavya';
-let randi=Math.floor(Math.random()*name.length);
+let name = "Kavya";
+let randi = Math.floor(Math.random() * name.length);
 console.log(name[randi]);
--- Strings/script.js
+++ Strings/script.js
@@ -1,6 +1,6 @@
-let str1="Strive not to be success, ";
-let str2="but rather to be valued.";
-console.log(str1+str2);
+let str1 = "Strive not to be success, ";
+let str2 = "but rather to be valued.";
+console.log(str1 + str2);
 console.log(str1.concat(str2));
 /*
 Partitioning String
@@ -12,32 +12,32 @@ substring(start,end)
 substr(start,end)
  */
 
-console.log(str1.slice(0,6));
-console.log(str1.slice(0,5));
+console.log(str1.slice(0, 6));
+console.log(str1.slice(0, 5));
 
-console.log(str2.substring(2,5));
-console.log(str1.slice(1,1));//start==end returns empty string
-console.log(str1.substr(2,10));
-console.log(str1.replace('Strive','Aim'));
+console.log(str2.substring(2, 5));
+console.log(str1.slice(1, 1)); //start==end returns empty string
+console.log(str1.substr(2, 10));
+console.log(str1.replace("Strive", "Aim"));
 console.log(str1.toUpperCase());
 console.log(str2.toLowerCase());
-let text="   Hello world";
+let text = "   Hello world";
 console.log(text.trim());
-text=" Hello world";
+text = " Hello world";
 console.log(text.trimStart());
-text="Hello world    ";
+text = "Hello world    ";
 console.log(text.trimEnd());
 
-let bill='Rs.10';
+let bill = "Rs.10";
 console.log(bill.length);
-console.log(bill.padEnd(7,'0'));
-console.log(bill.padStart(8,'s'));
+console.log(bill.padEnd(7, "0"));
+console.log(bill.padStart(8, "s"));
 console.log(str1.charAt(1));
 console.log(str1.charCodeAt(1));
-console.log(str1.indexOf('e'));
-console.log(str1.lastIndexOf('e'));
-console.log(str2.lastIndexOf('z'));
-console.log(str1.search('not'));
+console.log(str1.indexOf("e"));
+console.log(str1.lastIndexOf("e"));
+console.log(str2.lastIndexOf("z"));
+console.log(str1.search("not"));
 console.log(str1.search("values"));
-console.log(str1.includes('not'));
-console.log(str1.startsWith('S'));
+console.log(str1.includes("not"));
+console.log(str1.startsWith("S"));
