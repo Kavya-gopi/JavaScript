5e66e8cd21ee4efea79da0a66865a9e5ac93afe7
Kavya-gopi <kavijanu1121@gmail.com>
2024-02-17 18:59:20 +0530

    FD calculator

--- if-else/FDRate calculator/script.js
+++ if-else/FDRate calculator/script.js
@@ -4,18 +4,14 @@
 7-9 months-6.8%
 >=10 months-7%
  */
-const result=document.getElementById('result');
+const result = document.getElementById("result");
 function calculate() {
   const months = Number(document.getElementById("input").value);
   //console.log(input);
-  let rate=0;
-  if(months<3)
-    rate=5.8;
-  else if(months>=3 && months<=6)
-    rate=6.5;
-  else if(months>=7 && months<=9)
-    rate=6.8;
-  else
-    rate=7;
-  result.innerHTML=`Your interest rate is ${rate} %`;
+  let rate = 0;
+  if (months < 3) rate = 5.8;
+  else if (months >= 3 && months <= 6) rate = 6.5;
+  else if (months >= 7 && months <= 9) rate = 6.8;
+  else rate = 7;
+  result.innerHTML = `Your interest rate is ${rate} %`;
 }
