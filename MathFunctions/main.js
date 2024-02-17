161fcf849a8359fc32987bb88ab75b36683816a9
Kavya-gopi <kavijanu1121@gmail.com>
2024-02-17 19:00:48 +0530

    MathFunction

--- MathFunctions/main.js
+++ MathFunctions/main.js
@@ -7,16 +7,12 @@ console.log(Math.floor(2.34));
 console.log(Math.round(6.54));
 console.log(Math.floor(6.6));
 console.log(Math.ceil(2.1));
-console.log(Math.PI);//Returns PI its a property of Math function
+console.log(Math.PI); //Returns PI its a property of Math function
 console.log(Math.sqrt(4));
-console.log(Math.abs(-1));//Returns negative to positive integer
-console.log(Math.E);// Returns Euler's number
-console.log(Math.min(23,45,78));//Returns minimum value
-console.log(Math.max(23,45,78));//Returns maximum value
-console.log(Math.pow(3,2))//(base,power)
-console.log(Math.random()*6);
-console.log(Math.floor(Math.random()*6)+1);
-
-
-
-
+console.log(Math.abs(-1)); //Returns negative to positive integer
+console.log(Math.E); // Returns Euler's number
+console.log(Math.min(23, 45, 78)); //Returns minimum value
+console.log(Math.max(23, 45, 78)); //Returns maximum value
+console.log(Math.pow(3, 2)); //(base,power)
+console.log(Math.random() * 6);
+console.log(Math.floor(Math.random() * 6) + 1);
