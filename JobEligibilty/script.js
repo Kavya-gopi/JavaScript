0d401ba291e888f9e7a673bbfb7088f5aafc95e0
Kavya-gopi <kavijanu1121@gmail.com>
2024-02-17 19:03:43 +0530

    JobEligibility

--- JobEligibilty/script.js
+++ JobEligibilty/script.js
@@ -1,19 +1,16 @@
+let result = document.getElementById("result");
+const displayCriteria = () => {
+  const op = document.getElementsByName("Qualification");
+  console.log(op);
+  for (let i = 0; i < op.length; i++) {
+    if (op[i].checked) {
+      // document.getElementById('result').innerHTML=op[i].value;
+      let edu = op[i].value;
 
-let result=document.getElementById('result');
-const displayCriteria=()=>{
-    const op=document.getElementsByName('Qualification');
-    console.log(op);
-    for(let i=0;i<op.length;i++){
-        if(op[i].checked){
-            // document.getElementById('result').innerHTML=op[i].value;
-            let edu=op[i].value
-            
-            console.log(edu);
-            if(edu=='High School' || edu=='UG')
-               result.innerHTML=`Your are Eligible to apply for this Job`;
-            else
-               result.innerHTML= `Not Eligible`;
-    
-        }
+      console.log(edu);
+      if (edu == "High School" || edu == "UG")
+        result.innerHTML = `Your are Eligible to apply for this Job`;
+      else result.innerHTML = `Not Eligible`;
     }
-}
+  }
+};
